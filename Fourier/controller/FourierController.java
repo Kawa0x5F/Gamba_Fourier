1bd53ffbda67e61ea07f1148794a2cfd89498504
okamototaisuke <otaisuke420@gmail.com>
2025-06-06 09:29:21 +0000

    スペルミスとメニューの関数を修正しました

--- Fourier/controller/FourierController.java
+++ Fourier/controller/FourierController.java
@@ -1,8 +1,7 @@
 package Fourier.controller;
 
 public class FourierController {
-    public static boolean Keepdate = false; // データを保存するのを制御するための変数
+    public static boolean Keepdata = false; // データを保存するのを制御するための変数
     public static boolean Indata = false; // データを入力するのを制御するための変数
     public static boolean Respectrum = false; // スペクトルの削除を制御をするための変数
-
 }
