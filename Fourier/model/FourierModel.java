4bcfbe783c59866b3b424106056ddd6f7458286c
Kawa_ <yuyux0910@gmail.com>
2025-06-13 09:12:59 +0000

    feature: Viewへの通知のための処理の実装と、Double配列とComplex配列の変換を実装

--- Fourier/model/FourierModel.java
+++ Fourier/model/FourierModel.java
@@ -1,5 +1,20 @@
 package Fourier.model;
 
-public class FourierModel {
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
 
+public class FourierModel {
+    private final PropertyChangeSupport support = new PropertyChangeSupport(this);
+
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        support.addPropertyChangeListener(listener);
+    }
+
+    public void removePropertyChangeLisener(PropertyChangeListener listener) {
+        support.removePropertyChangeListener(listener);
+    }
+
+    protected void firePropertyChange(String propertyName, Object oldValue, Object newValue) {
+        support.firePropertyChange(propertyName, oldValue, newValue);
+    }
 }
