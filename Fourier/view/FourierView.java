f523b37e47c56ce921a5c244559e325356a26f9a
Kawa_ <yuyux0910@gmail.com>
2025-06-13 09:14:00 +0000

    feature: Modelからの通知を受け取るための処理を実装

--- Fourier/view/FourierView.java
+++ Fourier/view/FourierView.java
@@ -1,5 +1,33 @@
 package Fourier.view;
 
-public class FourierView {
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 
+import javax.swing.SwingUtilities;
+
+import Fourier.model.FourierModel;
+
+public abstract class FourierView implements PropertyChangeListener {
+    private final FourierModel model;
+
+    public FourierView(FourierModel model) {
+        this.model = model;
+
+        // 自分自身をモデルのリスナーとして登録
+        this.model.addPropertyChangeListener(this);
+    }
+
+    /**
+     * モデルへの参照を小クラスに提供するためのメソッド
+     */
+    protected FourierModel getModel() {
+        return this.model;
+    }
+
+    @Override
+    public void propertyChange(PropertyChangeEvent e) {
+        updateView();
+    }
+
+    protected abstract void updateView();
 }
--- Fourier/view/FourierView1D.java
+++ Fourier/view/FourierView1D.java
@@ -4,20 +4,20 @@ import Fourier.model.FourierModel1D;
 import javax.swing.*;
 import Fourier.FourierData;
 
-public class FourierView1D {
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> {
-            createWindow("Chirp Signal", FourierData.dataChirpSignal());
-            createWindow("Sawtooth Wave", FourierData.dataSawtoothWave());
-            createWindow("Square Wave", FourierData.dataSquareWave());
-            createWindow("Sample Wave", FourierData.dataSampleWave());
-            createWindow("Triangle Wave", FourierData.dataTriangleWave());
-            createWindow("WAV Signal", WavReader.readWavFile("example.wav"));
-        });
+public class FourierView1D extends FourierView {
+
+    SignalWindow window;
+
+    public FourierView1D(FourierModel1D model) {
+        super(model);
     }
 
-    private static void createWindow(String title, double[] data) {
-        SignalWindow window = new SignalWindow(title, data);
-        window.setVisible(true);
+    private void createWindow(String title, double[] data) {
+        this.window = new SignalWindow(title, data);
+        this.window.setVisible(true);
+    }
+
+    @Override
+    protected void updateView() {
     }
 }
