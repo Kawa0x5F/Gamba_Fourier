5d84ec861980dae973ce8012ca3afe15f6dc53ef
Kawa_ <yuyux0910@gmail.com>
2025-06-25 14:34:00 +0000

    feature: スペクトル再計算に必要なメソッド・変数を定義

--- Fourier/controller/FourierController1D.java
+++ Fourier/controller/FourierController1D.java
@@ -1,40 +1,52 @@
 package Fourier.controller;
 
 import Fourier.model.FourierModel1D;
-import Fourier.model.FourierModel2D;
 import java.awt.Point;
+import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.InputEvent;
 
-public class FourierController1D extends FourierController {
+public class FourierController1D extends MouseAdapter {
 
-	private FourierModel1D fourierModel1D = new FourierModel1D();
+    private FourierModel1D fourierModel1D;
 
-	Point dragchange = null;
+    public FourierController1D(FourierModel1D model) {
+        this.fourierModel1D = model;
+    }
 
-	private boolean isAltDown(MouseEvent sensing) {
-		return (sensing.getModifiersEx() & InputEvent.ALT_DOWN_MASK) != 0;
-	}
+    private boolean isAltDown(MouseEvent sensing) {
+        return (sensing.getModifiersEx() & InputEvent.ALT_DOWN_MASK) != 0;
+    }
 
-	public void mouseClicked(MouseEvent sensing) {
-		dragchange = sensing.getPoint();
-		fourierModel1D.computeFromMousePoint(dragchange, isAltDown(sensing));
-	}
+    @Override
+    public void mouseClicked(MouseEvent sensing) {
+        Point clickPoint = sensing.getPoint();
+        boolean altStatus = isAltDown(sensing);
+        System.out.println("Controller: Mouse Clicked at " + clickPoint + ", Alt key down: " + altStatus); // デバッグ出力
+        fourierModel1D.computeFromMousePoint(clickPoint, altStatus);
+    }
 
-	public void mousePressed(MouseEvent sensing) {
-		dragchange = sensing.getPoint();
-		fourierModel1D.computeFromMousePoint(dragchange, isAltDown(sensing));
-	}
+    @Override
+    public void mousePressed(MouseEvent sensing) {
+        Point pressPoint = sensing.getPoint();
+        boolean altStatus = isAltDown(sensing);
+        System.out.println("Controller: Mouse Pressed at " + pressPoint + ", Alt key down: " + altStatus); // デバッグ出力
+        fourierModel1D.computeFromMousePoint(pressPoint, altStatus);
+    }
 
-	public void mouseDragged(MouseEvent sensing) {
-		dragchange = sensing.getPoint();
-		fourierModel1D.computeFromMousePoint(dragchange, isAltDown(sensing));
+    @Override
+    public void mouseDragged(MouseEvent sensing) {
+        Point dragPoint = sensing.getPoint();
+        boolean altStatus = isAltDown(sensing);
+        System.out.println("Controller: Mouse Dragged at " + dragPoint + ", Alt key down: " + altStatus); // デバッグ出力
+        fourierModel1D.computeFromMousePoint(dragPoint, altStatus);
+    }
 
-	}
-
-	public void mouseReleased(MouseEvent sensing) {
-		dragchange = sensing.getPoint();
-		fourierModel1D.computeFromMousePoint(dragchange, isAltDown(sensing));
-	}
-
+    @Override
+    public void mouseReleased(MouseEvent sensing) {
+        Point releasePoint = sensing.getPoint();
+        boolean altStatus = isAltDown(sensing);
+        System.out.println("Controller: Mouse Released at " + releasePoint + ", Alt key down: " + altStatus); // デバッグ出力
+        fourierModel1D.computeFromMousePoint(releasePoint, altStatus);
+    }
 }
